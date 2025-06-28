9ad232c83a93e55e20fbe7c7e4238130c610d571
igargaditya <igargaditya@gmail.com>
2025-06-29 01:02:06 +0530

    Final keyword Uses

---
+++ Final_Keyword.java
@@ -0,0 +1,20 @@
+class Final {
+    public static final double pie = 3.14;
+    public static final double e;
+    public static final int age;
+
+    public Final() {
+        age = 18;
+    }
+
+    static {
+        e = 2.67;
+    }
+
+}
+
+public class Final_Keyword {
+    public static void main(String[] args) {
+
+    }
+}
